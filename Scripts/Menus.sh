c9c0bae4e19eed457a8f8a2fd7f0524b0f3599d6
iannai <116035930+iannaiiannai@users.noreply.github.com>
2024-08-14 14:08:08 -0300

    Si

---
+++ Scripts/Menus.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source ./variableINI.sh
+
+
+opcion=$((0))
+while [ $opcion -ne 4 ]
+do
+    echo ""
+    echo "--------------------------------"
+    echo "| Bienvenido al menú principal |"
+    echo "--------------------------------"
+    echo "      1. Usuarios"
+    echo "      2. Bases de datos"
+    echo "      3. Tablas"
+    echo "      4. Salir"
+    read -p "Seleccione uno de los menús: " opcion
+    echo ""
+
+    case $opcion in
+        1) ./Menus/Usuarios.sh
+        ;;
+        2) ./Menus/Bases_de_datos.sh
+        ;;
+        3) ./Menus/Tablas.sh
+        ;;
+        4) echo "-- Hasta luego --"
+           echo ""
+        exit
+        ;;
+        *) echo Seleccione una opción válida
+        ;;
+    esac
+    
+done
---
+++ Scripts/Menus/Usuarios.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source Funciones/FUsuarios.sh
+
+    echo ""
+    echo "----------------------------------"
+    echo "| Bienvenido al menú de usuarios |"
+    echo "----------------------------------"
+
+opcion=$((0))
+while [ $opcion -ne 6 ]
+do
+    echo "      1. Crear un usuario"
+    echo "      2. Eliminar un usuario"
+    echo "      3. Añadir permisos a un usuario"
+    echo "      4. Revocar permisos a un usuario"
+    echo "      5. Modificar un usuario"
+    echo "      6. Volver"
+    read -p "Seleccione uno de los menus: " opcion
+    echo ""
+
+    case $opcion in
+        1) 
+        ;;
+        2) 
+        ;;
+        3) 
+        ;;
+        4) 
+        ;;
+        5) 
+        ;;
+        6) exit
+        ;;
+        *) echo Seleccione una opción válida
+        ;;
+    esac
+    
+done
